7c1dc8fea42f6bb092faa4901cccae3a8a39ef87
Artem Golovin <joe.falange@yandex.ru>
2025-01-23 17:14:06 +0300

    feat/initial commit

---
+++ components/a-header/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<ul>
+  <li>1</li>
+  <li>2</li>
+  <li>3</li>
+  <li>4</li>
+  <li>5</li></ul>
+</template>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+
+<template>
+  <a-header />
+  <slot />
+</template>
+
+<style lang="scss"></style>
